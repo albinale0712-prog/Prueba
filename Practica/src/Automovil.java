34962980b53bf5ff8edb5a3ac2a3db46f2a9e4b1
albinale0712-prog <albinale0712@gmail.com>
2026-06-25 15:18:36 -0600

    Agregando clases Automovil y Principal

---
+++ Practica/src/Automovil.java
@@ -0,0 +1,141 @@
+import javax.swing.JOptionPane;
+
+public class Automovil {
+
+    private int id;
+    private String marca;
+    private String modelo;
+    private double precio;
+    private String tipoTransmision;
+
+    public void ingresarAutomovil(int pos, Automovil autos) {
+        if (pos >= autos.length) {
+            System.out.println("Vector lleno");
+            return;
+        }
+        setId(Integer.parseInt(JOptionPane.showInputDialog("Digite el ID")));
+        setMarca(JOptionPane.showInputDialog("Digite la marca"));
+        setModelo(JOptionPane.showMessageDialog(null,"Digite el modelo"););
+        setPrecio(Double.parseDouble(JOptionPane.showInputDialog("Digite el precio")));
+        setTipoTransmision(JOptionPane.showInputDialog("Digite el tipo de transmisión"));
+        autos[pos] = new Automovil(getId(),getMarca(),getPrecio(), getModelo(), getTipoTransmision());
+    }
+
+    public void ordenarAutos(Automovil autos[], int contador) {
+        for (int i = 0; i < contador - 1; i++)
+            for (int j = i + 1; j < contador; j++) {
+                if (autos[i].getPrecio() < autos[j].getPrecio()) {
+                    Automovil temp = autos[i];
+                    autos[i] = autos[j];
+                    autos[i] = temp;
+                }
+            }
+        System.out.println("Vector ordenado correctamente");
+
+
+        public int posicionMasCaro(Automovil autos[], int contador) {
+            if (contador == 0) {
+                return -1;
+            }
+
+            int posicion = 0;
+            double mayor = autos[0].getPrecio();
+
+            for (int i = 1; i < contador; i++)
+                if (autos[i].getPrecio() > mayor) {
+                    mayor = autos[i].getPrecio();
+                    posicion = i;
+                }
+
+            return i;
+        }
+
+        public void imprimirAutos(Automovil autos[], int contador) {
+            if (contador == 0) {
+                System.out.println("No hay automóviles registrados");
+                return;
+            }
+
+            for (int i = 0; i < contador; i++) {
+                System.out.println("ID: " + autos[i].getId());
+                System.out.println("\nMarca: " + autos[i].getMarca());
+                System.out.println("\nModelo: " + autos[i].getModelo());
+                System.out.println("\nPrecio: ₡" + autos[i].getPrecio());
+                System.out.println("\nTransmisión: " + autos[i].getTipoTransmision());
+                System.out.println("\n----------------------\n");
+            }
+        }
+
+        public void imprimirMayores35000000(Automovil autos[], int contador) {
+            boolean enc=false;
+
+            for (int i = 0; i < contador; i++)
+                if (autos.getPrecio() > 35000000) {
+                    System.out.println("ID: " + autos[i].getId());
+                    System.out.println("\nMarca: " + autos[i].getMarca());
+                    System.out.println("\nModelo: " + autos[i].getModelo());
+                    System.out.println("\nPrecio: ₡" + autos[i].getPrecio());
+                    System.out.println("\nTransmisión: " + autos[i].getTipoTransmision());
+                    System.out.println("\n----------------------\n")
+                    enc=true;
+                }
+
+            if (enc==false)
+                System.out.println("No existen automóviles con precio mayor a ₡35.000.000");
+        }
+
+    public Automovil {
+        }
+
+    public Automovil(int id, String marca, String modelo,
+        double precio, String tipoTransmision) {
+
+            this.id = id;
+            this.marca = marca;
+            this.modelo = modelo;
+            this.precio = precio;
+            this.tipoTransmision = tipoTransmision;
+        }
+
+        // GET Y SET
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getMarca() {
+            return marca;
+        }
+
+        public void setMarca(String marca) {
+            this.marca = marca;
+        }
+
+        public String getModelo() {
+            return modelo;
+        }
+
+        public void setModelo(String modelo) {
+            this.modelo = modelo;
+        }
+
+        public double getPrecio() {
+            return precio;
+        }
+
+        public void setPrecio(double precio) {
+            this.precio = precio;
+        }
+
+        public String getTipoTransmision() {
+            return tipoTransmision;
+        }
+
+        public void setTipoTransmision(String tipoTransmision) {
+            this.tipoTransmision = tipoTransmision;
+
+        }
---
+++ Practica/src/Principal.java
@@ -0,0 +1,61 @@
+import javax.swing.JOptionPane;
+
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        Automovil autos [] = Automovil[3];
+        int contador = 0;
+        int opcion=0;
+
+        Automovil obj = new Automovil();
+        while (opcion <= 5){
+            int opcion = Integer.parseInt(JOptionPane.showInputDialog(
+                    "MENÚ\n\n"
+                            + "1. Ingresar automóvil\n"
+                            + "2. Ordenar de mayor a menor precio\n"
+                            + "3. Posición del automóvil más caro\n"
+                            + "4. Imprimir automóviles\n"
+                            + "5. Automóviles mayores a ₡35.000.000\n"
+                            + "6. Salir"));
+
+            switch (opcion)
+            case 1:
+                obj.ingresarAutomovil(autos, contador);
+                if (contador < autos.length) {
+                    contador++;
+                }
+                break;
+
+            case 2:
+                obj.ordenarAutos(autos, contador);
+                break;
+
+            case 3:
+                int pos = posicionMasCaro(autos, contador);
+                if (pos == -1)
+                    System.out.println("No hay automóviles registrados");
+                else {
+                    System.out.println("La posición del automóvil más caro es: " + pos);
+                    System.out.println("El automóvil es: " + autos[pos].getModelo());
+                    break;
+
+                    case 4:
+                        obj.imprimirAutos(autos, contador);
+                        break;
+
+                    case 5:
+                        obj.imprimirMayores35000000(autos, contador);
+                        break;
+
+                    case 6:
+                        System.out.println("Fin del programa");
+                        break;
+
+                    default:
+                        System.out.println("Opción inválida");
+                }
+        } //cierra while
+    }
+}
